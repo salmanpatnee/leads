7106f1bda137ef937253d321dc8634a85fefb64f
salmanpatnee <salmanpatni92@gmail.com>
2026-01-20 16:45:42 +0500

    Improve detail page UI

---
+++ public/build/assets/Spinner.vue_vue_type_script_setup_true_lang-VAVIEH-h.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-BkvyfrpA.js";import{L as r}from"./loader-circle-CBwgT5Rm.js";import{d as n,g as c,u as s,C as t,o as l}from"./app-CGCrkDAk.js";const f=n({__name:"Spinner",props:{class:{}},setup(a){const e=a;return(i,p)=>(l(),c(s(r),{role:"status","aria-label":"Loading",class:t(s(o)("size-4 animate-spin",e.class))},null,8,["class"]))}});export{f as _};
---
+++ public/build/assets/useFormControl-_38DnNvm.js
@@ -0,0 +1,1 @@
+import{f as r}from"./index-ibSaU8ej.js";import{c as t,Y as e}from"./app-CGCrkDAk.js";function n(o){return t(()=>e(o)?!!r(o)?.closest("form"):!0)}export{n as u};
